02b1d7ce24c4066ec3870394b4fdd49f17bd57bc
WilsonFernandes <wilsonfernandes31470@gmail.com>
2023-06-20 11:57:04 +0200

    first commit

---
+++ src/main/java/com/wf/demo/payload/User.java
@@ -0,0 +1,11 @@
+package com.wf.demo.payload;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}
---
+++ src/test/java/com/wf/demo/SpringbootKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
